94d4d17fa79ae2bb4e12c5d8a5cdaedd31f35bd3
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-05-19 19:57:16 +0200

    Bendo grid section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { FaHome } from 'react-icons/fa';
 
+import Grid from '@/components/Grid';
 import Hero from '@/components/Hero';
 import { FloatingNav } from '@/components/ui/FloatingNavbar';
 
@@ -17,6 +18,7 @@ export default function Home() {
           ]}
         />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
