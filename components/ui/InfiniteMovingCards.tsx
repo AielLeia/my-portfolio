4a32ead7597a1ae07d25abd11286ef0516c5c78c
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-07-04 20:44:46 +0200

    Amélioration de la navigation, sur les mobile, le défilement s'arrête lorsque l'utilisateur reste appuier sur l'écran.

--- components/ui/InfiniteMovingCards.tsx
+++ components/ui/InfiniteMovingCards.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from '@/utils/cn';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 export const InfiniteMovingCards = ({
   items,
@@ -20,13 +20,40 @@ export const InfiniteMovingCards = ({
   pauseOnHover?: boolean;
   className?: string;
 }) => {
-  const containerRef = React.useRef<HTMLDivElement>(null);
-  const scrollerRef = React.useRef<HTMLUListElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const scrollerRef = useRef<HTMLUListElement>(null);
+
+  const [isTouching, setIsTouching] = useState(false);
+  const [start, setStart] = useState(false);
 
   useEffect(() => {
     addAnimation();
+    window.addEventListener('touchstart', handleTouchStart);
+    window.addEventListener('touchend', handleTouchEnd);
+
+    return () => {
+      window.removeEventListener('touchstart', handleTouchStart);
+      window.removeEventListener('touchend', handleTouchEnd);
+    };
   }, []);
-  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    // Restart animation when isTouching state changes
+    if (containerRef.current && scrollerRef.current) {
+      if (isTouching) {
+        containerRef.current.style.setProperty(
+          '--animation-play-state',
+          'paused'
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          '--animation-play-state',
+          'running'
+        );
+      }
+    }
+  }, [isTouching]);
+
   function addAnimation() {
     if (containerRef.current && scrollerRef.current) {
       const scrollerContent = Array.from(scrollerRef.current.children);
@@ -43,6 +70,7 @@ export const InfiniteMovingCards = ({
       setStart(true);
     }
   }
+
   const getDirection = () => {
     if (containerRef.current) {
       if (direction === 'left') {
@@ -58,6 +86,7 @@ export const InfiniteMovingCards = ({
       }
     }
   };
+
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === 'fast') {
@@ -69,6 +98,15 @@ export const InfiniteMovingCards = ({
       }
     }
   };
+
+  const handleTouchStart = () => {
+    setIsTouching(true);
+  };
+
+  const handleTouchEnd = () => {
+    setIsTouching(false);
+  };
+
   return (
     <div
       ref={containerRef}
@@ -80,9 +118,10 @@ export const InfiniteMovingCards = ({
       <ul
         ref={scrollerRef}
         className={cn(
-          ' flex min-w-full shrink-0 gap-16 py-4 w-max flex-nowrap',
-          start && 'animate-scroll ',
-          pauseOnHover && 'hover:[animation-play-state:paused]'
+          'flex min-w-full gap-16 py-4 w-max flex-nowrap',
+          start && 'animate-scroll',
+          pauseOnHover && 'md:hover:[animation-play-state:paused]',
+          isTouching && 'hover:[animation-play-state:paused]'
         )}
       >
         {items.map((item, idx) => (
@@ -100,16 +139,16 @@ export const InfiniteMovingCards = ({
                 aria-hidden="true"
                 className="user-select-none -z-1 pointer-events-none absolute -left-0.5 -top-0.5 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
               ></div>
-              <span className=" relative z-20 leading-[1.6] text-white text-sm md:text-lg font-normal">
+              <span className="relative z-20 leading-[1.6] text-white text-sm md:text-lg font-normal">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                   <div className="flex flex-col gap-1">
-                    <span className=" text-xl leading-[1.6] text-white font-normal">
+                    <span className="text-xl leading-[1.6] text-white font-normal">
                       {item.name}
                     </span>
-                    <span className=" text-sm leading-[1.6] text-white-200 font-normal">
+                    <span className="text-sm leading-[1.6] text-white-200 font-normal">
                       {item.title}
                     </span>
                   </div>
