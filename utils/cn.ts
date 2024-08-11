d11f4ae1eaef9f73a0f1d72e07498a0e2e38b151
Ismaël MOHAMED BOUH <ismaelmedbouh@outlook.fr>
2024-08-11 17:14:22 +0200

    Ajout d'une documentation pour la fonction cn.

--- utils/cn.ts
+++ utils/cn.ts
@@ -1,6 +1,10 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Permet de dynamiquement cancatener des classes tailwind.
+ * @param inputs
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
